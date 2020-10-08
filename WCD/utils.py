5abd15fccd5357f131e41cf40f0dde01a2bfdb49
Gaojie Jin <40000098+Alexkael@users.noreply.github.com>
2020-10-08 15:25:08 +0100

    Update utils.py

--- WCD/utils.py
+++ WCD/utils.py
@@ -95,34 +95,6 @@ def get_cifar10(net):
      
     return trn, tst
 
-def get_cifar100(net):
-
-    nb_classes = 100
-
-    (X_train, y_train), (X_test, y_test) = keras.datasets.cifar100.load_data()
-    
-    if net=='CNN':  
-        X_train = np.reshape(X_train, [X_train.shape[0], 32, 32, 3]).astype('float32') / 255
-        X_test = np.reshape(X_test , [X_test.shape[0] , 32, 32, 3]).astype('float32') / 255
-    elif net=='FCN':
-        X_train = np.reshape(X_train, [X_train.shape[0], -1]).astype('float32') / 255.
-        X_test  = np.reshape(X_test , [X_test.shape[0] , -1]).astype('float32') / 255.
-        
-
-    #X_train = X_train * 2.0 - 1.0
-    #X_test  = X_test  * 2.0 - 1.0
-
-    Y_train = keras.utils.to_categorical(y_train, nb_classes).astype('float32')
-    Y_test  = keras.utils.to_categorical(y_test, nb_classes).astype('float32')
-
-    Dataset = namedtuple('Dataset',['X','Y','y','nb_classes'])
-    trn = Dataset(X_train, Y_train, y_train, nb_classes)
-    tst = Dataset(X_test , Y_test, y_test, nb_classes)
-
-    del X_train, X_test, Y_train, Y_test, y_train, y_test
-     
-    return trn, tst
-
 def get_svhn(net):
     # Returns two namedtuples, with MNIST training and testing data
     #   trn.X is training data
